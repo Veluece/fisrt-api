e5374c399aa0c413c5902db43a53b5d5e3fdc08e
Vadivel R <velu20503531@gmail.com>
2025-09-15 03:16:47 -0700

    route structure

--- main.js
+++ main.js
@@ -4,8 +4,19 @@ const app = express()
 const port = 6969
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.json({msg:"Hello World!"});
+});
+
+// CRUD functionality of api
+ //R - Reading
+app.get('/api',()=>{})
+
+//C - creating
+app.post('/api',()=>{})
+//U - updating
+app.put('/api/:id',()=>{})
+//D - deleting
+app.delete('/api/:id',()=>{})
 
 app.listen(port, () => {
   console.log(`The server is running at http://localhost:${port}`)
